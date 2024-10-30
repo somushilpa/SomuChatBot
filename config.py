d1f66c6708335fa31c69c5b4715365ff0419fcba
somushilpa <somushilpa94@gmail.com>
2024-10-30 08:48:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6664582540"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+BOT_TOKEN = getenv("BOT_TOKEN", "7313059877:AAGiCX8wPk-F6G00xoDeoNR-dQZ4HIRS1I0")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Somu:Somu@somu.xbkiklu.mongodb.net/?retryWrites=true&w=majority&appName=Somu")
+OWNER_ID = "7070591202"
+SUPPORT_GRP = "somueditingzone"
+UPDATE_CHNL = "somueditingzone"
+OWNER_USERNAME = "alone_somu6"
 
